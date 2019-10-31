85796cd824657f21c95a3fcd6820e673334f0ec2
张卓诚 <zhangzhuocheng@gagogroup.com>
2019-10-31 16:41:09 +0800

    feat(app/bundle.d.ts): add d.ts

---
+++ app/bundle.d.ts
@@ -0,0 +1,20 @@
+export declare const init: (
+    canvas: HTMLCanvasElement,
+    mapConfig: {
+        mapUrl: string,
+        spritesheetUrl: string,
+        rippleMaterialUrl: string,
+        locations: Location[],
+    },
+    onLoaded:(destroy: ()=>void)=>any
+)=>void
+
+
+export interface Location{
+    model: string,
+    position: [number, number, number],
+    scale: [number, number, number],
+    key: string,
+    name: string,
+    namePosition: [number, number, number],
+}
